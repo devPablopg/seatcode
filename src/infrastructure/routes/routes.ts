173001abb4a01cb75ea0582d60586830d579a1a0
ppardogo <pablo.pardo@code.seat>
2025-04-01 12:29:43 +0200

    update routes

--- src/infrastructure/routes/routes.ts
+++ src/infrastructure/routes/routes.ts
@@ -1,13 +1,13 @@
 export enum Routes {
-    HOME = 'seatcode/',
-    ABOUT = 'seatcode/#/about',
-    OURSOLUTIONS = 'seatcode/#/oursolutions',
-    WORKWITHUS = 'seatcode/#/workwithus',
-    PARTNERSHIP = 'seatcode/#/partnership',
-    TERMS_OF_USE = 'seatcode/#/terms-of-use',
-    PRIVACY_POLICY = 'seatcode/#/privacy-policy',
-    INTEGRITY = 'seatcode/#/integrity-and-compliance',
+    HOME = '/',
+    ABOUT = '#/about',
+    OURSOLUTIONS = '#/oursolutions',
+    WORKWITHUS = '#/workwithus',
+    PARTNERSHIP = '#/partnership',
+    TERMS_OF_USE = '#/terms-of-use',
+    PRIVACY_POLICY = '#/privacy-policy',
+    INTEGRITY = '#/integrity-and-compliance',
     ENVIRONMENTAL_POLICY = 'https://www.seat.com/company/environmental-policy.html',
-    WHISTLEBLOWER = 'seatcode/#/whistleblower-channel',
+    WHISTLEBLOWER = '#/whistleblower-channel',
     CODE_LINKEDIN = 'https://www.linkedin.com/company/seatcode/posts/?feedView=all'
 }

68e8b031a54fa8bde13cf88cd328f807f0100649
ppardogo <pablo.pardo@code.seat>
2025-04-01 13:08:00 +0200

    updatesç

--- src/modules/notFound/infrastructure/i18n/NotFoundText.ts
+++ src/modules/notFound/infrastructure/i18n/NotFoundText.ts
@@ -3,7 +3,7 @@ import { NotFoundTranslations } from "./typing";
 
 export const NotFoundText: Translation<NotFoundTranslations> = {
   en: {
-    messageFirstPart: "Sorry! we couldn't find",
+    messageFirstPart: "Sorry, we couldn't find",
     messageSecondPart: "what you are looking for",
     messageThirdPart: "Looks that this page doesn't exist",
     backHome: "take me back home"

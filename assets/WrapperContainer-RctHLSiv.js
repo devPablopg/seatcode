ad3b861d18319e111268a57dc5f4b21f36cc0c62
ppardogo <pablo.pardo@code.seat>
2025-04-02 07:40:07 +0200

    Updates

--- assets/WrapperContainer-RctHLSiv.js
+++ assets/WrapperContainer-RctHLSiv.js
@@ -1,4 +1,4 @@
-import{L as t}from"./LocaleService-C19YxyPP.js";import{d as o}from"./index-D_KJ_VRd.js";const n=o.section`
+import{L as t}from"./LocaleService-C19YxyPP.js";import{d as o}from"./index-CCMKRyxT.js";const n=o.section`
   main {
     box-sizing: border-box;
     min-height: 100vh;
--- assets/index-D47c1UOU.js
+++ assets/index-D47c1UOU.js
@@ -1,4 +1,4 @@
-import{d as a,m as l,j as t,R as g}from"./index-D_KJ_VRd.js";import{L as i,W as n,T as x,M as w}from"./LocaleService-C19YxyPP.js";import{S as $}from"./link-Bt9cBOrY.js";var f=(e=>(e.BOLD="bold",e.NORMAL="normal",e))(f||{});const y="INTRO",v=a.section`
+import{d as a,m as l,j as t,R as g}from"./index-CCMKRyxT.js";import{L as i,W as n,T as x,M as w}from"./LocaleService-C19YxyPP.js";import{S as $}from"./link-CSEmMD3R.js";var f=(e=>(e.BOLD="bold",e.NORMAL="normal",e))(f||{});const y="INTRO",v=a.section`
   display: flex;
   position: relative;
   flex-wrap: wrap;
